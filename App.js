8b0ec3c235947aa3a33043cd4a66fabd8f6d4a50
Ayush Sinha <ayushsinhajsr20@gmail.com>
2023-03-27 01:58:51 +0530

    Complete

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StyleSheet,View, Text } from 'react-native'
+import React, {useEffect} from 'react'
+import SplashScreen from 'react-native-splash-screen'
+
+const App = () => {
+  useEffect(()=>{
+    SplashScreen.hide()
+  })
+  
+  return (
+    <View>
+      <Text style={styles.Title}>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  Title:{
+    color: "#000000",
+    fontSize: 40,
+    fontWeight: "900",
+    alignSelf: "center",
+    marginTop: 20,
+  }
+})
+
+export default App
